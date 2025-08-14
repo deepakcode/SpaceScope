29e8cdbb0156ad2225aa9b634b31f2aa115c13b2
deepakcode <blrhyd0@gmail.com>
2025-08-15 00:15:22 +0530

    working 3-Gemini-01-option

--- SpaceScope/FilterSettings.swift
+++ SpaceScope/FilterSettings.swift
@@ -3,5 +3,5 @@ import Foundation
 struct FilterSettings {
     var hideSmallFiles: Bool = true
     var hideHiddenFiles: Bool = false
-    var greySmallFiles: Bool = true // New setting for greying out files < 1GB
+    var greySmallFiles: Bool = true 
 }
