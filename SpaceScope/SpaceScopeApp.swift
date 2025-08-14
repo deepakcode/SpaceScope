5b26531a9b128a6860e9d07c9462c9ca5cced057
deepakcode <blrhyd0@gmail.com>
2025-08-14 17:22:05 +0530

    Initial Commit

---
+++ SpaceScope/SpaceScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceScopeApp.swift
+//  SpaceScope
+//
+//  Created by Deepak Vishwakarma on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SpaceScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
